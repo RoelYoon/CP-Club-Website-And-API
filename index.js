c357a15f22617e87de98dba92905c0450f1be128
RoelYoon <roel.yoon@ggq.gg>
2023-08-09 16:39:32 +0900

    Fixed port value

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
 app.get("/api/test/",(req,res)=>{res.send("test complete")})
-app.listen(8008, "0.0.0.0");
+app.listen(8080, "10.138.0.2");
