5c5faf2c1dd786246738d4e9cdb242a5e4d44fb7
RoelYoon <roel.yoon@ggq.gg>
2023-08-09 16:10:17 +0900

    Corrected IP Address of VM

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
 app.get("/api/test/",(req,res)=>{res.send("test complete")})
-app.listen(8008,'208.67.222.222');
+app.listen(8008,'34.83.209.8');
