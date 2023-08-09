284d2741f8fb4f2de2406efd7d77724cee3b4b00
RoelYoon <roel.yoon@ggq.gg>
2023-08-10 05:59:48 +0900

    Test

--- baekjoonAPI.js
+++ baekjoonAPI.js
@@ -1,5 +1,7 @@
-const base = "api/baekjoon/"
+const base = "/api/baekjoon/"
 module.exports = {apiCall : async (req,res)=>{
+    console.log(req.url)
+    console.log(req.url.substring(base.length));
     switch(req.url.substring(base.length)){
         case 'get_status_message/':
             res.send('api implementation in development').status(200);
