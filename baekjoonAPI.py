c69dfd9df1ef07dcd2495dc048214efedd29be10
RoelYoon <roel.yoon@ggq.gg>
2023-08-10 04:18:06 +0900

    Updated

---
+++ baekjoonAPI.py
@@ -0,0 +1,5 @@
+from baekjoon import boj
+user = "roelyoon"
+print(boj.get_correct_q(user,{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}))
+
+#'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
