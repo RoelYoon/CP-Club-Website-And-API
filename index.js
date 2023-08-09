170b2ea6f8a7f18292d430f527b606bfd493a7e5
RoelYoon <roel.yoon@ggq.gg>
2023-08-10 05:25:03 +0900

    API Structure & Routing

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const api = require("./api.js");
 const path = require("path");
+const baekjoon = require("./baekjoonAPI.js");
+const solvedac = require("./solvedacAPI.js");
+
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
-app.get("/api/test/",(req,res)=>(res.send("test complete")));
-app.listen(8080, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 8080")});
+
+app.get("/",(req,res)=>(res.sendFile(path.join(__dirname,"index.html"))));
+app.get("/api/baekjoon/",async (req,res)=>(await baekjoon.apiCall(req,res)));
+app.get("/api/solvedac/",async (req,res)=>(await solvedac.apiCall(req,res)));
+app.get('*',(req,res)=>(res.send("404 boiii").status(404)));
+
+app.listen(5000);
+//app.listen(8080, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 8080")});
