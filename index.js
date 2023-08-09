403cf33c60518a5de20aaf10b89f27c7e858be3d
RoelYoon <roel.yoon@ggq.gg>
2023-08-10 05:47:27 +0900

    Paths fixed

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
 
 app.get("/",(req,res)=>(res.sendFile(path.join(__dirname,"./index.html"))));
-app.get("/api/baekjoon/",async (req,res)=>(await baekjoon.apiCall(req,res)));
-app.get("/api/solvedac/",async (req,res)=>(await solvedac.apiCall(req,res)));
-app.get('*',(req,res)=>(res.send("404 boiii").status(404)));
-
-app.listen(8080, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 8080")});
+app.get("/api/baekjoon/*",async (req,res)=>(await baekjoon.apiCall(req,res)));
+app.get("/api/solvedac/*",async (req,res)=>(await solvedac.apiCall(req,res)));
+app.get('*',(req,res)=>{res.send("404 boiii").status(404);});
+app.listen(5000);
+//app.listen(8080, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 8080")});
