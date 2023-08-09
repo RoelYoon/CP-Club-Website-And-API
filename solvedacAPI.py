036512723bce6d24f78e8404c11b4e966701b00f
RoelYoon <roel.yoon@ggq.gg>
2023-08-10 05:26:21 +0900

    Testing

---
+++ solvedacAPI.py
@@ -0,0 +1,3 @@
+from baekjoon import solvedac
+user = "roelyoon"
+print(solvedac.get_tier(user,{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}))
