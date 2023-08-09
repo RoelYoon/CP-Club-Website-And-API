b96cf1bca465f73de17c2693f3e7fa1092d5cde1
RoelYoon <roel.yoon@ggq.gg>
2023-08-09 21:31:30 +0900

    Fix

--- api.js
+++ api.js
@@ -3,4 +3,15 @@ const testCall = async () => {
     const myJson = await response.json();
     return myJson;
 }
+const call = () => {
+const http = require("http");
+const req = http.request("http://hasura.io/blog/top-psql-commands-and-flags-you-need-to-know-postgresql/",(res)=>{
+    res.setEncoding('utf8');
+    res.on('data', function(data) {
+        console.log(data);
+    });
+})
+req.end();}
 module.exports.testCall=testCall; 
+module.exports.call=call; 
+              
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const api = require("./api.js");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
-app.get("/api/test/",(req,res)=>{res.send("test complete")})
-app.listen(8080, "10.138.0.2");
+app.get("/api/test/",(req,res)=>(res.send("test complete")));
+app.listen(8080, "10.138.0.2", ()=>{console.log("Listening at IP 34.83.209.8 port 8080")});
