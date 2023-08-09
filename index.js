0cdbf2823be50f3a0cba23541efc77a5c8ab9e94
RoelYoon <roel.yoon@ggq.gg>
2023-08-09 16:11:59 +0900

    Test

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
 app.get("/api/test/",(req,res)=>{res.send("test complete")})
-app.listen(8008,'34.83.209.8');
+app.listen(8008,()=>{});
