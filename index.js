082f68fee0c0a059ccdf6c6b72669b16dce8843c
RoelYoon <roel.yoon@ggq.gg>
2023-08-09 16:19:26 +0900

    Fixed VM IP address

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")));
 app.get("/api/test/",(req,res)=>{res.send("test complete")})
-app.listen(8008,()=>{});
+app.listen(8008,'34.83.209.8');
