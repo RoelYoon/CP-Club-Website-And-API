35c7dc6ff185b354d89c02cf5ecd0fcc76deb14f
RoelYoon <roel.yoon@ggq.gg>
2023-08-11 01:54:16 +0900

    Removed old files

--- baekjoonAPI.py
+++
@@ -1,5 +0,0 @@
-from baekjoon import boj
-user = "roelyoon"
-print(boj.get_correct_q(user,{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}))
-
-#'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
--- solvedacAPI.py
+++
@@ -1,3 +0,0 @@
-from baekjoon import solvedac
-user = "roelyoon"
-print(solvedac.get_tier(user,{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}))
