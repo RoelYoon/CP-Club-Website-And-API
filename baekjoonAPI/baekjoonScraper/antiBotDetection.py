1342d89eb3c6a695dcf76a387b6e958cd0173dd1
RoelYoon <roel.yoon@ggq.gg>
2023-08-11 01:47:27 +0900

    Added file for configuring anti bot detection parameters

---
+++ baekjoonAPI/baekjoonScraper/antiBotDetection.py
@@ -0,0 +1,6 @@
+# Add additional params or proxies to help with scrapers avoiding bot detection. Also consider implementing user-agent or proxy rotation
+protectionArgs={
+    'headers':{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'},
+    'params':{},
+    'proxies':{}
+}
