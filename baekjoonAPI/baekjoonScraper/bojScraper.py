29a8152248c085977b4d2cce57b4c6fdfad79c93
RoelYoon <roel.yoon@ggq.gg>
2023-08-12 22:23:42 +0900

    Updated old API paths

--- baekjoonAPI/baekjoonScraper/bojScraper.py
+++ baekjoonAPI/baekjoonScraper/bojScraper.py
@@ -3,8 +3,8 @@ import antiBotDetection
 import sys
 action = sys.argv[1]
 user = sys.argv[2]
-if action == "get_status_message":
-    print(boj.get_status_message(user,**antiBotDetection.protectionArgs))
+if action == "get_stats":
+    print(boj.get_stats(user,**antiBotDetection.protectionArgs))
 elif action == "get_rank":
     print(boj.get_rank(user,**antiBotDetection.protectionArgs))
 elif action == "get_correct_qs":
@@ -15,6 +15,6 @@ elif action == "get_unsolved_qs":
     print(boj.get_unsolved_qs(user,**antiBotDetection.protectionArgs))
 elif action == "get_unsolved_q":
     print(boj.get_unsolved_q(user,**antiBotDetection.protectionArgs))
-elif action == "get_submit_time":
-    print(boj.get_submit_time(user,**antiBotDetection.protectionArgs))
+elif action == "get_submissions":
+    print(boj.get_submissions(user,**antiBotDetection.protectionArgs))
 sys.__stdout__.flush()
